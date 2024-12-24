88b67b9d391263bd76983c7257daf9a3c1ae2f84
Omerba31 <benarieo@post.bgu.ac.il>
2024-12-24 10:49:49 +0200

    waiting for meni's answer

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClientBuilder;
+import com.amazonaws.services.elasticmapreduce.model.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class Main {
+    public static AWSCredentialsProvider credentialsProvider;
+    public static AmazonS3 S3;
+    public static AmazonEC2 ec2;
+    public static AmazonElasticMapReduce emr;
+
+    public static int numberOfInstances = 10;
